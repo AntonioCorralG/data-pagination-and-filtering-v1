a0ff131ea5b0d27a92a328d3884e3bd0966d7ba4
AntonioCorralG <97654161+AntonioCorralG@users.noreply.github.com>
2022-06-01 15:14:44 -0700

    completed project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,66 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const showPage = (list, page) => {
+  const startIndex = page * 9 - 9;
+  const endIndex = page * 9;
 
+  const studentList = document.querySelector(".student-list");
+
+  studentList.innerHTML = " ";
+
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      let studentItem = `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+       </li>`;
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+    }
+  }
+};
+
+showPage(data,1);
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+const addPagination = (list) => {
+  const numOfPages = Math.ceil(list.length / 9);
+  let linkList = document.querySelector(".link-list");
 
+  linkList.innerHTML = "";
 
+  for (let i = 1; i <= numOfPages; i++) {
+    const button = `
+    <li>
+      <button type="button">${[i]}</button>
+    </li>`;
+    linkList.insertAdjacentHTML("beforeend", button);
 
-// Call functions
+    let active = document.querySelector('li button')
+    active.className = 'active';
+  }
+
+  linkList.addEventListener('click', ( (e) =>{
+      if (e.target.tagName === 'BUTTON') {
+         let unactive = document.querySelector('.active');
+         unactive.className = '';
+         e.target.className = 'active';
+         showPage(list, e.target.textContent);
+
+      }
+  }));
+};
+
+addPagination(data);
